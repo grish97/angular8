4854589700bdbea264ef49b5c6c14ff2ad3df35b
Grish <grisha@myperfectidea.com>
2019-10-01 16:35:32 +0400

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/pages/home/home.component';
+import {BlogComponent} from './components/pages/blog/blog.component';
 
 
-const routes: Routes = [];
+const renderComponent = (Component: any, isLayout?: boolean) => {
+  console.log(isLayout);
+  return Component;
+};
+
+
+const routes: Routes = [
+  {
+    path: ``,
+    component: renderComponent(HomeComponent, true)
+  },
+  {
+    path: `blog`,
+    component: BlogComponent
+  },
+  {
+    path: `our-team`,
+    component: BlogComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { BlogComponent } from './components/pages/blog/blog.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { OurTeamComponent } from './components/pages/our-team/our-team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    HeaderComponent,
+    FooterComponent,
+    OurTeamComponent
   ],
   imports: [
     BrowserModule,
